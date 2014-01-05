39223fdb6a9cfba6e2dee17dc2b76297af99c695
dglambert <dglambert@outlook.com>
2014-01-04 23:11:20 -0500

    Created SLN for project

---
+++ SB Sound Board/SB Sound Board/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SB_Sound_Board.Resources;
+
+namespace SB_Sound_Board
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
